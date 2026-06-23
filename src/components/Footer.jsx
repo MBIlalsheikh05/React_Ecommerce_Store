62800ed90adfaee4ec29f28aefd77bd050c061fd
Muhammad Bilal Sheikh <140721272+MBIlalsheikh05@users.noreply.github.com>
2026-06-23 17:50:43 +0500

    Changess

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -46,7 +46,7 @@ const Footer = () => {
       </div>
       <div className='mt-8 border-t border-r-gray-700 pt-4'>
         <div className='conatiner mx-auto flex flex-col md:flex-row justify-between items-center'>
-          <p>&copy; 2024 e-Shop|Taha Shaikh | All rights reserved.</p>
+          <p>&copy; 2024 e-Shop|Bilal Sheikh | All rights reserved.</p>
           <div className='flex space-x-4 mt-4 md:mt-0'>
             <a href='' className='hover:underline'>Privacy Policy</a>
             <a href='' className='hover:underline'>Terms & Conditions</a>
